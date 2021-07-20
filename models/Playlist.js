4ca9021dc2d9a43976c085ce3c2008d7764f63a5
Paul Kim <paulwkim30@gmail.com>
2021-07-19 21:26:42 -0700

    start playlist model

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,52 +1,14 @@
-const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
-const passport = require('passport');
+const Schema = mongoose.Schema;
 
-const Playlist = require('../../models/Playlist');
-const validateTweetInput = require('../../validation/playlists');
-
-router.get('/', (req, res) => {
-    Playlist.find()
-        .sort({ date: -1 })
-        .then(playlists => res.json(playlists))
-        .catch(err => res.status(404).json({ noplaylistsfound: 'No playlists found' }));
+const PlaylistSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  song: [
+    
+  ]
 });
 
-router.get('/user/:user_id', (req, res) => {
-    Playlist.find({user: req.params.user_id})
-        .sort({ date: -1 })
-        .then(playlists => res.json(playlists))
-        .catch(err =>
-            res.status(404).json({ noplaylistsfound: 'No playlists found from that user' }
-        )
-    );
-});
-
-router.get('/:id', (req, res) => {
-    Playlist.findById(req.params.id)
-        .then(playlist => res.json(playlist))
-        .catch(err =>
-            res.status(404).json({ noplaylistfound: 'No playlist found with that ID' })
-        );
-});
-
-router.post('/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validatePlaylistInput(req.body);
-        
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
-  
-      const newPlaylist = new Playlist({
-        text: req.body.text,
-        user: req.user.id
-      });
-  
-      newPlaylist.save().then(playlist => res.json(playlist));
-    }
-  );
-
-  module.exports = router;
+module.exports = Playlist = mongoose.model('Playlist', PlaylistSchema)
