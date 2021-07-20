690a4a8c440be5ea71713825da0527f8d4222ea4
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-07-20 14:49:05 -0500

    whatever

--- frontend/src/util/playlist_api_util.js
+++ frontend/src/util/playlist_api_util.js
@@ -3,10 +3,11 @@
 import axios from 'axios';
 
 export const getPlaylists = () => {
-  return axios.get('/api/playlists')
+  return axios.get(`/api/playlists`)
 };
 
 export const getUserPlaylists = id => {
 
   return axios.get(`/api/playlists/user/${id}`)
 };
+
