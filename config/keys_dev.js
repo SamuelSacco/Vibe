69eb9a6d81ae4a0b5bdfe78d655ffc20359edb6e
rgltalatala <63977819+rgltalatala@users.noreply.github.com>
2021-07-22 15:42:49 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://paul:1DyKvVmrreSfmu9Y@cluster0.avf6c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    secretOrKey: 'OSJhC`%V4cH,V$zblUfjQ(#I4Faoo['
-    //Make sure this is your own unique string
-}
