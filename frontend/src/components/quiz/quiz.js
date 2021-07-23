59d0ddcb6f5c76309de2c2e469dd106f88e9efee
Paul Kim <paulwkim30@gmail.com>
2021-07-22 20:36:24 -0700

    fix session issue

--- frontend/src/components/quiz/quiz.js
+++ frontend/src/components/quiz/quiz.js
@@ -96,10 +96,11 @@ export function Quiz (props) {
 						<h2 className='question-text'>{questions[currentQuestion].questionText}</h2>
 					</div>
 					<div className='quiz-answers'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
+						{questions[currentQuestion].answerOptions.map((answerOption, idx) => (
 							<button 
 								onClick={() => handleAnswerOptionClick(answerOption.activity, answerOption.mood)}
 								className='quiz-answer'
+                key={idx}
 							>
 								<div className='quiz-answer-text'>
 									{answerOption.answerText}
