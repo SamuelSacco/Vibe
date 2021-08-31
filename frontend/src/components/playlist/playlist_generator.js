9ae257bbcce07f6803efcc9d11b1af147763c10f
rgltalatala <rgltalatala@gmail.com>
2021-08-31 11:59:20 -0400

    Add volume warning

--- frontend/src/components/playlist/playlist_generator.js
+++ frontend/src/components/playlist/playlist_generator.js
@@ -24,26 +24,27 @@ function PlaylistGenerator(props) {
       {
         widgetId && loading === false ? 
         <div className='playlist-wrapper'>
-        <h1 className='playlist-show-header'>Your vibe is...</h1>
-        <iframe 
-          title="playlist-widget" 
-          src={`https://open.spotify.com/embed/playlist/${widgetId}`} 
-          width="300" 
-          height="380" 
-          frameBorder="0" 
-          allowtransparency="true" 
-          allow="encrypted-media"
-          className='playlist-show-widget'
-        ></iframe>
-        <div className='playlist-func-button-wrapper'>
-          <SavePlaylist 
-          currentUserId={props.currentUser.id} 
-          // playlist={songs.listOfSongsFromAPI} 
-          createPlaylist={props.createPlaylist} 
-          widgetId={widgetId}
-          className='playlist-func-button'
-          ownProps={props.ownProps}
-        />
+          <h1 className='playlist-show-header'>Your vibe is...</h1>
+          <iframe 
+            title="playlist-widget" 
+            src={`https://open.spotify.com/embed/playlist/${widgetId}`} 
+            width="300" 
+            height="380" 
+            frameBorder="0" 
+            allowtransparency="true" 
+            allow="encrypted-media"
+            className='playlist-show-widget'
+          ></iframe>
+          <div className='playlist-func-button-wrapper'>
+            <SavePlaylist 
+            currentUserId={props.currentUser.id} 
+            // playlist={songs.listOfSongsFromAPI} 
+            createPlaylist={props.createPlaylist} 
+            widgetId={widgetId}
+            className='playlist-func-button'
+            ownProps={props.ownProps}
+          />
+          <div>*Volume may be high, so please lower your volume before playing any songs*</div>
         </div>
       </div>
       : <div className='playlist-loading'>
